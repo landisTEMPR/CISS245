2f102a1f14d4386320ec8545f937e8f8ea3196f1
landisTEMPR <bglandis1@cougars.ccis.edu>
2025-09-08 15:29:52 -0500

    reference varibales

---
+++ notes/09-08-2025/09-08-2025.cpp
@@ -0,0 +1,26 @@
+// Pass by reference practice
+
+#include <iostream>
+
+
+int main()
+{
+    int i = 42;
+    int & j = i;
+
+    std::cout << i << ' ' << j << '\n';
+
+    i = 0;
+    std::cout << i << ' ' << j << '\n';
+    
+    j = 5;
+    std::cout << i << ' ' << j << '\n';
+
+    int & k = j;
+    std::cout << i << ' ' << j << ' ' << k << '\n';
+
+    k = 8;
+    std::cout << i << ' ' << j << ' ' << k << '\n';
+
+    return 0;
+}
