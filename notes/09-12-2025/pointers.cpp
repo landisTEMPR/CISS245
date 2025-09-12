8de618ae607983701a38e91b03104b5bcd2ca6f7
landisTEMPR <bglandis1@cougars.ccis.edu>
2025-09-12 14:54:48 -0500

    pointers 2

--- notes/09-12-2025/pointers.cpp
+++ notes/09-12-2025/pointers.cpp
@@ -1,3 +1,17 @@
+//----------------------------------------------------------------------------
+// NOTES:
+//   Reference variables are actually pointers.
+//
+//   There is a part of your memory that you cannot touch. This is called the
+//   heap. We've been using the stack up until this point. The heap is many
+//   times larger than the stack.
+//   Pointers allow you to access this part of the memory. If you want to make
+//   your program faster and use more memory you must use pointers  to access
+//   the heap. This is important for data-structures in CISS350. Data-
+//   structures allow you to organize large amounts of data and access them
+//   quickly and in an organized way.
+//----------------------------------------------------------------------------
+
 #include <iostream>
 
 int main()
