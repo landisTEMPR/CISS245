1262b295a50d60da6594d2640cda2b6e8d28d2a4
landisTEMPR <bglandis1@cougars.ccis.edu>
2025-09-18 14:55:43 -0500

    isprime

---
+++ notes/09-18-2025/nextprime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+bool isprime(int n)
+{
+    
+}
+
+
+void nextprime(int & x)
+{
+    
+}
+
+
+int main()
+{
+    int x = 7;
+    nextprime(&x);
+    std::cout << x << '\n';
+    
+    return 0;
+}
