493915b19a44444ff33c351e840790c2c6941706
landisTEMPR <bglandis1@cougars.ccis.edu>
2025-09-30 18:10:10 -0500

    assignment 3 string comparison

---
+++ a/a03/a03q04/mystring.h
@@ -0,0 +1,14 @@
+// File : mystring.h
+// Author : Brysen Landis
+
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+int str_len(char []);
+int str_cmp(char [], char []);
+int str_chr(char [], char c);
+int str_str(char [], char []);
+void str_cpy(char [], char []);
+void str_cat(char [], char []);
+
+#endif
