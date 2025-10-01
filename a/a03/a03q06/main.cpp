0abeec33f016575db8c64616da661f5deb3f493b
landisTEMPR <bglandis1@cougars.ccis.edu>
2025-10-01 13:20:55 -0500

    q6

---
+++ a/a03/a03q06/main.cpp
@@ -0,0 +1,123 @@
+// File : main.cpp
+// Author : Brysen Landis
+
+#include <iostream>
+#include <limits>
+#include "mystring.h"
+
+
+const int MAX_BUF = 1024;
+
+void test_str_cmp()
+{
+    char s[MAX_BUF];
+    char t[MAX_BUF];
+
+    std::cin.getline(s, MAX_BUF);
+    std::cin.getline(t, MAX_BUF);
+    std::cout << str_cmp(s, t) << std::endl;
+
+    return;
+}
+
+
+void test_str_cpy()
+{
+    char x[MAX_BUF];
+    char y[MAX_BUF];
+    std::cin.getline(y, MAX_BUF);
+    
+    str_cpy(x, y);
+    std::cout << x << std::endl;
+
+    return;
+}
+
+
+void test_str_chr()
+{
+    char x[MAX_BUF];
+    char c;
+
+    std::cin.getline(x, MAX_BUF);
+    std::cin >> c;
+    int i = str_chr(x, c);
+    std::cout << i << '\n';
+
+    return;
+}
+
+
+void test_str_str()
+{
+    char x[MAX_BUF];
+    char y[MAX_BUF];
+
+    std::cin.getline(x, MAX_BUF);
+    std::cin.getline(y, MAX_BUF);
+
+    std::cout << str_str(x, y) << std::endl;
+
+    return;
+}
+
+
+void test_str_lower()
+{
+    char x[MAX_BUF];
+    char y[MAX_BUF];
+
+    std::cin.getline(y, MAX_BUF);
+    
+    str_lower(x, y);
+    std::cout << x << std::endl;
+
+    return;
+}
+
+
+void test_str_tok();
+{
+    char x[MAX_BUF];
+    char y[MAX_BUF];
+    char delimiters[MAX_BUF] = " ,.";
+
+    std::cin.getline(y, MAX_BUF);
+    bool b = str_tok(x, y, delimiters);
+    std::cout << b << ' ' << x << ' ' << y << std::endl;
+
+    return;
+    
+}
+
+
+int main()
+{
+    int i = 0;
+    std::cin >> i;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    
+    switch (i)
+    {
+        case 0:
+            test_str_cmp();
+            break;
+        case 1:
+            test_str_cpy();
+            break;
+        case 2:
+            test_str_chr();
+            break;
+        case 3:
+            test_str_str();
+            break;
+        case 4:
+            test_str_lower();
+            break;
+        case 5:
+            test_str_tok();
+            break;
+    }
+    
+    return 0;
+}
---
+++ a/a03/a03q06/mystring.h
@@ -0,0 +1,16 @@
+// File : mystring.h
+// Author : Brysen Landis
+
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+int str_len(char []);
+int str_cmp(char [], char []);
+int str_chr(char [], char c);
+int str_str(char [], char []);
+void str_cpy(char [], char []);
+void str_cat(char [], char []);
+void str_lower(char [], char[]);
+void str_tok(char [], char [], char []);
+
+#endif
