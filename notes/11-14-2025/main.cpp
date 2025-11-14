2b5bcbffc12eb9d3a6303acddc77e1d74d057ca5
landisTEMPR <bglandis1@cougars.ccis.edu>
2025-11-14 15:29:10 -0600

    recursion

--- notes/11-14-2025/main.cpp
+++ notes/11-14-2025/main.cpp
@@ -23,6 +23,7 @@ int max(int x[], int start, int end)
     }
 }
 
+
 int linsrch(int x[], int start, int end, int target)
 {
     if (start == end)
@@ -32,21 +33,54 @@ int linsrch(int x[], int start, int end, int target)
     }
     else
     {
+        // recursive case
         if (x[start] == target) return start;
         else return linsrch(x, start + 1, end, target);
     }
 }
 
+int sum(int x[], int start, int end)
+{
+    if (start == end) return 0;
+    else
+    {
+        return sum(x, start + 1, end) + x[start];
+    }
+}
 
+
+int count(int x[], int start, int end, int target)
+{
+    if (start == end) return 0;
+    else
+    {
+        return count(x, start + 1, end, target) + (x[start] == target ? 1 : 0);
+    }
+}
+
+
+bool ispalindrome(char x[], start, end)
+{
+    if (start >= end) return true;
+    else
+    {
+        if (x[start] != x[end - 1]) return false;
+        else ispalindrome(x, start + 1, end - 1);
+    }
+}
 // SO ITS LIKE A HOUSE OF CARDS WHERE THE FUNCTION KEEPS CALLING ITSELF AND BUILDS LIKE A HOUSE OF CARDS AND THEN THE BASE CASE IS LIKE SOMEONE KNOCKING IT OVER AND THE SCOPES THAT WERE OPEN FOR EACH CALL OF THE FUNCTION START COLLAPSING FROM THE MOST RECENT TO OLDEST CALL.
 
 int main()
 {
-    int x[] = {5, 3, 1, 2, 4, 6, 9, 8, 7};
+    int x[] = {5, 3, 1, 2, 4, 6, 2, 2, 9, 8, 7, 2};
     int m = max(x, 1, 7);
     std::cout << "MAX: " << m << '\n';
 
     std::cout << "TARGET FOUND AT: " << linsrch(x, 1, 7, 2) << '\n';
+
+    std::cout << "COUNTING: " << count(x, 0, 15, 2) << '\n';
+
+    std::cout << "SUM: " << sum(x, 0, 11) << '\n';
 
     return 0;
 }
